c097eb2202002c87ae7133fb7415e679f1d64bcf
Aaron Ma <aaronhma@gmail.com>
2020-06-09 12:02:59 -0700

    Complete implementation of Linear Search

---
+++ algorithms/search/linear_search.cpp
@@ -0,0 +1,49 @@
+/**
+ * This file is a part of the algorithm implementation collection.
+ *
+ * This file is under the MIT LICENSE.
+ * In using it, you agree to abide by the LICENSE
+ * at all times.
+ *
+ * @file binary_search_tree.cpp
+ *  This implementations the linear search algorithm.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+#define PB push_back
+
+/* Linear search implementation */
+class LinearSearch
+{
+public:
+  LinearSearch() {}
+  ~LinearSearch() {}
+  void push(int x)
+  {
+    this->arr.PB(x);
+  }
+
+  bool search(int x)
+  {
+    for (int i = 0; i < this->arr.size(); i++)
+    {
+      if (i == x)
+        return true;
+    }
+
+    return false;
+  }
+
+private:
+  vector<int> arr;
+};
+
+int main()
+{
+  /* Linear search test code */
+
+  return 0;
+}
