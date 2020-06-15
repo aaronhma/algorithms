c3a00fc023457090e9091325aeae6e2b34b201c4
Aaron Ma <aaronhma@gmail.com>
2020-06-15 10:25:55 -0700

    Create macros.h

---
+++ data_structures/include/macros.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// TODO: Remove header guards
+#ifndef MACROS_H_
+#define MACROS_H_
+
+#define PB push_back
+#define print(message) std::cout << message << std::endl
+
+#endif // MACROS_H_
