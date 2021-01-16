0bdadcc5950e0fd5879d362f003ba13c931f86c4
Aaron Ma <aaronhma@gmail.com>
2021-01-16 14:08:25 -0800

    Fix Sieve of Eratosthenes bug

--- math/primes-and-factors/sieve.cpp
+++ math/primes-and-factors/sieve.cpp
@@ -12,12 +12,12 @@ int main()
 
   vector<int> sieve(n + 1, 0);
 
-  for (int i = 2; i < n; i++)
+  for (int i = 2; i <= n; i++)
   {
     if (sieve[i])
       continue;
 
-    for (int j = 2 * i; j < n; j += i)
+    for (int j = 2 * i; j <= n; j += i)
       sieve[j] = 1;
   }
 
