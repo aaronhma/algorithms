633e5a5fb4e2e644ab4425eca422a5a17e763831
Aaron Ma <aaronhma@gmail.com>
2021-01-30 14:54:39 -0800

    Create max-subarray.cpp

---
+++ data-structure/linear/array/src/max-subarray/max-subarray.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, -2, 3, 4};
+
+    int local = nums[0], global = nums[0];
+    
+    for (int i = 1; i < nums.size(); i++) {
+        local = max(local + nums[i], nums[i]);
+        global = max(global, local);
+    }
+    
+    cout << global << "\n";
+}
