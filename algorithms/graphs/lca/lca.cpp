a33996ec68106e9c589e5ca8d596f25b78048a20
Aaron Ma <aaronhma@gmail.com>
2021-08-14 11:40:48 -0700

    Update graph notes

---
+++ algorithms/graphs/lca/lca.cpp
@@ -0,0 +1,278 @@
+// TODO: https://www.spoj.com/problems/LCA/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<str> vstr;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+#define f first
+#define s second
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define PB push_back
+#define sz(x) (int)x.size()
+#define rtn return
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define LB lower_bound
+#define UB upper_bound
+#define BINS binary_search
+#define IT(x) for (auto it = begin(x); it != end(x); it++)
+#define RIT(x) for (auto it = rbegin(x); it != rend(x); it++)
+#define FORE(i, a, b) for (int i = a; i < b; i++)
+#define FORI(i, a, b) for (int i = a; i <= b; i++)
+#define each(i, x) for (auto &i : x)
+
+const int MOD = 1e9 + 7;
+const int INF_INT = 2147483647;
+const long INF_L = 1000000000000000003;
+const ll INF_LL = 9223372036854775807;
+const ld PI = acos((ld)-1);
+
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+// Returns ceil(a / b) (b != 0)
+int ceildiv(int a, int b)
+{
+  rtn a / b + !(a < 0 || a % b == 0);
+}
+
+// INPUT
+template <class T>
+void read(T &x) { cin >> x; }
+
+void read(double &x)
+{
+  string t;
+  read(t);
+  x = stod(t);
+}
+void read(ld &x)
+{
+  string t;
+  read(t);
+  x = stold(t);
+}
+
+template <class T>
+void read(complex<T> &x)
+{
+  T a, b;
+  read(a, b);
+  x = cd(a, b);
+}
+
+template <class T1, class T2>
+void read(pair<T1, T2> &p) { read(p.f, p.s); }
+
+template <class T>
+void read(vector<T> &a)
+{
+  FORE(i, 0, sz(a))
+  read(a[i]);
+}
+
+template <class Arg, class... Args>
+void read(Arg &first, Args &...rest)
+{
+  read(first);
+  read(rest...);
+}
+
+// OUTPUT
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &a)
+{
+  os << '{' << a.f << ", " << a.s << '}';
+  return os;
+}
+
+template <class T>
+ostream &printArray(ostream &os, const T &a, int SZ)
+{
+  os << '{';
+  FORE(i, 0, SZ)
+  {
+    if (i)
+    {
+      os << ", ";
+    }
+    os << a[i];
+  }
+  os << '}';
+  return os;
+}
+
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &a)
+{
+  return printArray(os, a, sz(a));
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &a)
+{
+  os << vector<T>(all(a));
+  return os;
+}
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const map<T1, T2> &a)
+{
+  os << vector<pair<T1, T2>>(all(a));
+  return os;
+}
+
+template <class T>
+void pr(const T &x) { cout << x << '\n'; }
+template <class Arg, class... Args>
+void pr(const Arg &first, const Args &...rest)
+{
+  cout << first << ' ';
+  pr(rest...);
+}
+
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+// Fast I/O
+void setIO(str S = "", bool use_txt = false)
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  if (!S.empty())
+  {
+    if (use_txt)
+    {
+      freopen((S + ".txt").c_str(), "r", stdin);
+      freopen((S + ".txt").c_str(), "w", stdout);
+    }
+    else
+    {
+      freopen((S + ".in").c_str(), "r", stdin);
+      freopen((S + ".out").c_str(), "w", stdout);
+    }
+  }
+}
+
+// Custom hash for unordered_map and unordered_set (works only for integers)
+struct custom_hash
+{
+  static uint64_t splitmix64(uint64_t x)
+  {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()(uint64_t x) const
+  {
+    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+
+vi time_in, time_out;
+int timer, l;
+
+void dfs(vvi &graph, vvi &parent, int node, int prev) {
+  time_in[node] = ++timer;
+
+  parent[node][0] = prev;
+
+  FORI(i, 1, l)
+    parent[node][i] = parent[parent[node][i - 1]][i - 1];
+
+  for (int i : graph[node]) {
+    if (i != prev)
+      dfs(graph, parent, i, node);
+  }
+
+  time_out[node] = ++timer;
+}
+
+void preprocess(vvi &graph, vvi &parent, int root, int n) {
+  time_in.resize(n);
+  time_out.resize(n);
+  timer = 0;
+  dfs(graph, parent, root, -1);
+}
+
+// (u, v)
+bool is_ancestor(int u, int v) {
+  return time_in[u] <= time_in[v] && time_out[u] >= time_out[v];
+}
+
+int lca(vvi &parent, int u, int v) {
+  if (is_ancestor(u, v)) return u;
+  if (is_ancestor(v, u)) return v;
+
+  for (int i = l; i >= 0; i--) {
+    if (!is_ancestor(parent[u][i], v))
+      u = parent[u][i];
+  }
+
+  return parent[u][0];
+}
+
+int main()
+{
+  setIO(); // Disable this during interactive problems
+
+  int t, n, q, k, k1;
+  read(t);
+
+  FORI(cas_no, 1, t) {
+    read(n);
+
+    cout << "Case " << cas_no << ":\n";
+
+    l = ceil(log2(n));
+    vvi graph(n), parent(n, vi(l + 1));
+
+    preprocess(graph, parent, 0, n);
+
+    FORE(i, 0, n) {
+      read(q);
+
+      while (q--) {
+        read(k);
+        graph[i].PB(k - 1);
+        graph[k - 1].PB(i);
+      }
+    }
+
+    read(q);
+
+    while (q--) {
+      read(k, k1);
+      k--; k1--;
+      cout << lca(parent, k, k1) << "\n";
+    }
+  }
+
+  return 0;
+}
+/**
+ * Steps to solve CP problems:
+ * 1) Carefully extract the important information from the problem.
+ * 2) Find out all the hidden information.
+ * 3) Read input & output & understand the question.
+ * 4) Visualize the problem.
+ *  a. Does my result output the expected output?
+ *  b. Does the idea you think works?
+ *  c. Check for edge cases.
+ * 5) Implementation.
+*/
