7d26879a43105981af722af46e6615040a1023cf
Aaron Ma <aaronhma@gmail.com>
2021-08-30 12:53:30 -0700

    Fix A*

--- algorithms/graphs/a-star-algorithm/a-star-algorithm.cpp
+++ algorithms/graphs/a-star-algorithm/a-star-algorithm.cpp
@@ -3,12 +3,27 @@
 using namespace std;
 
 typedef vector<vector<int>> vvi;
-typedef pair<int, int> Point;
 
 #define sz(x) (int)x.size()
 
 // 8-sided direction vector
 const int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+class Point {
+public:
+  int x, y;
+
+  Point(int X, int Y) : x(X), y(Y) {}
+
+  bool isValid(int &n, int &m) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+  }
+
+  bool operator==(const Point &other) {
+    return x == other.x && y == other.y;
+  }
+};
+
 
 struct Cell
 {
@@ -23,12 +38,12 @@ struct Cell
 double calculateHValue(int &new_x, int &new_y, Point &dest)
 {
   return sqrt(
-      (new_x - dest.first) * (new_x - dest.first) + (new_y - dest.second) * (new_y - dest.second));
+      (new_x - dest.x) * (new_x - dest.x) + (new_y - dest.y) * (new_y - dest.y));
 }
 
 void tracePath(vector<vector<Cell>> &cellDetails, Point &dest)
 {
-  int x = dest.first, y = dest.second;
+  int x = dest.x, y = dest.y;
 
   stack<Point> path;
 
@@ -45,7 +60,7 @@ void tracePath(vector<vector<Cell>> &cellDetails, Point &dest)
   while (!path.empty()) {
     Point p = path.top();
     path.pop();
-    cout << "-> (" << p.first << ", " << p.second << ") ";
+    cout << "-> (" << p.x << ", " << p.y << ") ";
   }
 
   cout << "\n";
@@ -55,7 +70,7 @@ void tracePath(vector<vector<Cell>> &cellDetails, Point &dest)
 void aStarSearch(vvi &arr, Point &start, Point &end, int n, int m)
 {
   // Base case 1: If the starting and ending vertices isn't valid, return
-  if (!(start.first >= 0 && start.second >= 0 && end.first >= 0 && end.second >= 0 && start.first < n && start.second < m && end.first < n && end.second < m && arr[start.first][start.second] && arr[end.first][end.second]))
+  if (!(start.isValid(n, m) && end.isValid(n, m) && arr[start.x][start.y] && arr[end.x][end.y]))
   {
     cout << "No path exists.\n";
     return;
@@ -64,7 +79,7 @@ void aStarSearch(vvi &arr, Point &start, Point &end, int n, int m)
   // Base case 2: If the starting and ending vertices is the same, return
   if (start == end)
   {
-    cout << "Path exists.\n(" << start.first << ", " << start.second << ")\n";
+    cout << "Path exists.\n(" << start.x << ", " << start.y << ")\n";
     return;
   }
 
@@ -72,16 +87,16 @@ void aStarSearch(vvi &arr, Point &start, Point &end, int n, int m)
   vector<vector<Cell>> cellDetails(n, vector<Cell>(m)); // Details of cell (i, j)
 
   // Update starting cell value
-  cellDetails[start.first][start.second].f = 0.0;
-  cellDetails[start.first][start.second].g = 0.0;
-  cellDetails[start.first][start.second].h = 0.0;
-  cellDetails[start.first][start.second].parent_i = start.first;
-  cellDetails[start.first][start.second].parent_j = start.second;
+  cellDetails[start.x][start.y].f = 0.0;
+  cellDetails[start.x][start.y].g = 0.0;
+  cellDetails[start.x][start.y].h = 0.0;
+  cellDetails[start.x][start.y].parent_i = start.x;
+  cellDetails[start.x][start.y].parent_j = start.y;
 
   // {f, {i, j}}
   // f = g + h
   queue<pair<double, Point>> Q;
-  Q.push({0.0, {start.first, start.second}});
+  Q.push({0.0, {start.x, start.y}});
 
   while (!Q.empty())
   {
@@ -89,7 +104,7 @@ void aStarSearch(vvi &arr, Point &start, Point &end, int n, int m)
     pair<double, Point> node = Q.front();
     Q.pop();
 
-    int i = node.second.first, j = node.second.second;
+    int i = node.second.x, j = node.second.y;
     visited[i][j] = 1; // Mark current node as visited
 
     double gNew, hNew, fNew; // Variables to store the new g, h, and f value
@@ -98,41 +113,41 @@ void aStarSearch(vvi &arr, Point &start, Point &end, int n, int m)
     for (int k = 0; k < 8; k++)
     {
       // New neighbor index
-      int new_x = i + dx[k], new_y = j + dy[k];
+      Point neighbor(i + dx[k], j + dy[k]);
 
       // Make sure this neighbor is valid
-      if (new_x >= 0 && new_y >= 0 && new_x < n && new_y < m && arr[new_x][new_y])
+      if (neighbor.isValid(n, m) && arr[neighbor.x][neighbor.y])
       {
         // If we've reached the destination, backtrack to re-trace the path and return.
-        if (new_x == end.first && new_y == end.second)
+        if (neighbor.x == end.x && neighbor.y == end.y)
         {
-          cout << "Path exists.\nThe path from (" << start.first << ", " << start.second << ") to (" << end.first << ", " << end.second << ") is:\n";
-          cellDetails[new_x][new_y].parent_i = i;
-          cellDetails[new_x][new_y].parent_j = j;
+          cout << "Path exists.\nThe path from (" << start.x << ", " << start.y << ") to (" << end.x << ", " << end.y << ") is:\n";
+          cellDetails[neighbor.x][neighbor.y].parent_i = i;
+          cellDetails[neighbor.x][neighbor.y].parent_j = j;
           tracePath(cellDetails, end);
           return;
         }
 
         // If we haven't visited this neighbor, visit it.
-        if (!visited[new_x][new_y])
+        if (!visited[neighbor.x][neighbor.y])
         {
           double val = (dx[k] != 0 && dy[k] != 0) ? 1.414 : 1.0;
           gNew = cellDetails[i][j].g + val;          // movement cost from starting cell to current cell
-          hNew = calculateHValue(new_x, new_y, end); // estimated movement remaining
+          hNew = calculateHValue(neighbor.x, neighbor.y, end); // estimated movement remaining
           fNew = gNew + hNew;                        // f = g + h
 
           // If the new f is less than the previous f, select this cell for further exploration.
-          if (cellDetails[new_x][new_y].f > fNew)
+          if (cellDetails[neighbor.x][neighbor.y].f > fNew)
           {
             // Add this cell to queue for exploration
-            Q.push({fNew, {new_x, new_y}});
+            Q.push({fNew, {neighbor.x, neighbor.y}});
 
             // Update the details of this cell
-            cellDetails[new_x][new_y].f = fNew;
-            cellDetails[new_x][new_y].g = gNew;
-            cellDetails[new_x][new_y].h = hNew;
-            cellDetails[new_x][new_y].parent_i = i;
-            cellDetails[new_x][new_y].parent_j = j;
+            cellDetails[neighbor.x][neighbor.y].f = fNew;
+            cellDetails[neighbor.x][neighbor.y].g = gNew;
+            cellDetails[neighbor.x][neighbor.y].h = hNew;
+            cellDetails[neighbor.x][neighbor.y].parent_i = i;
+            cellDetails[neighbor.x][neighbor.y].parent_j = j;
           }
         }
       }
@@ -155,7 +170,7 @@ int main()
           {1, 1, 1, 0, 0, 0, 1, 0, 0, 1}};
 
   int n = sz(arr), m = sz(arr[0]);
-  Point start{8, 0}, end{0, 0};
+  Point start(8, 0), end(0, 0);
 
   aStarSearch(arr, start, end, n, m);
 
