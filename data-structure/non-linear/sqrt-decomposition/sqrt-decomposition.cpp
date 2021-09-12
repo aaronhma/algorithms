ea180af3f92a4d44af6ba85a9665fa519f978540
Matt Ma <bigmabig@gmail.com>
2021-09-12 13:09:24 -0700

    update sqrt

--- data-structure/non-linear/sqrt-decomposition/sqrt-decomposition.cpp
+++ data-structure/non-linear/sqrt-decomposition/sqrt-decomposition.cpp
@@ -20,13 +20,14 @@ int sqrtDecomposition(vector<int> arr, int ql, int qr) {
     for (int i = ql; i <= qr; i++)
       sum += arr[i];
   else {
+    // Query type 2 (see README.md) data before middle block
     // The blocks index contains the query left index. Add each value regardless of all or partial elements in the current block
     // end is the last index of the current blocks
     // (l + 1) * blockSize would be the start of the next blocks index
     for (int i = ql, end = (l + 1) * blockSize - 1; i <= end; i++)
       sum += arr[i];
 
-    // Middle blocks is optional, sometimes, it won't ever be called
+    // Add middle blocks is optional, sometimes, it won't ever be called
     // ex: l=0, r=1, this loop won't be called
     // ex: l=0, r=3,
     //  previous loop has taken all sum up value of blocks index 0
@@ -35,6 +36,7 @@ int sqrtDecomposition(vector<int> arr, int ql, int qr) {
     for (int i = l + 1; i <= r - 1; i++)
       sum += blocks[i];
 
+    // Query type 2 (see README.md) data after middle block
     // The blocks index contains the query right index. Add each value regardless of all or partial elements in the current block
     for (int i = r * blockSize; i <= qr; i++)
       sum += arr[i];
@@ -47,9 +49,15 @@ int main() {
   // Separated in 4 blocks and each block has a size of 4
   vector<int> arr{1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1};
 
-  int sum = sqrtDecomposition(arr, 3, 12); // 10
-  // Special case, both l and r are in the same blocks
-  int sum = sqrtDecomposition(arr, 0, 3); // 4
+  // Case 1: else case in the function
+  // int sum = sqrtDecomposition(arr, 3, 12); // 10
+
+  // Case 2: Special case, both l and r are in the same blocks
+  // int sum = sqrtDecomposition(arr, 0, 3); // 4
+
+  // Case 3: Update update individual array elements
+  arr[4] = 2;
+  int sum = sqrtDecomposition(arr, 3, 12); // 11
 
   cout << "sum: " << sum << "\n";
 
