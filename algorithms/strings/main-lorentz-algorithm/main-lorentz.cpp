d21f0408c781673d0ff99986a72d7a00aef1409f
Matt Ma <bigmabig@gmail.com>
2021-09-14 14:59:50 -0700

    main lorentz

--- algorithms/strings/main-lorentz-algorithm/main-lorentz.cpp
+++ algorithms/strings/main-lorentz-algorithm/main-lorentz.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 vector<pair<int, int>> repetitions;
 
-// Exact match to the `z_function` implementation
-vector<int> z_function(string const &s)
+// Exact match to the `z` implementation
+vector<int> z(string const &s)
 {
   int n = s.size(), l = 0, r = 0;
 
@@ -28,32 +28,17 @@ vector<int> z_function(string const &s)
   return z;
 }
 
-int get_z(vector<int> const &z, int i)
-{
-  // Check if it is out of the boundary
-  if (0 <= i && i < (int)z.size())
-    return z[i];
-  else
-    return 0;
-}
-
-void convert_to_repetitions(int shift, bool left, int i, int l, int k1, int k2)
-{
-  for (int l1 = max(1, l - k2); l1 <= min(l, k1); l1++)
-  {
-    if (left && l1 == l)
-      break;
-
-    int pos = shift + (left ? i - l1 : i - l - l1 + 1);
-
-    repetitions.emplace_back(pos, pos + 2 * l - 1);
-  }
+int getZValue(vector<int> const &z, int i) {
+  // if highest possible index is in range, return its value
+  if (0 <= i && i < (int)z.size()) return z[i];
+  else return 0;
 }
 
 // `O(n log n)` using divide-and-conquer algorithm
-void find_repetitions(string s, int shift = 0)
-{
+// shift: index position of the current process string
+void find_repetitions(string s, int shift = 0) {
   int n = s.size();
+
   if (n == 1)
     return;
 
@@ -61,61 +46,76 @@ void find_repetitions(string s, int shift = 0)
   int rightLen = n - leftLen;
 
   string leftStr = s.substr(0, leftLen);
-  string rightStr = s.substr(leftLen);
   string leftStrReverted(leftStr.rbegin(), leftStr.rend());
+
+  string rightStr = s.substr(leftLen);
   string rightStrReverted(rightStr.rbegin(), rightStr.rend());
 
-  // Finds all repetitions starting in the first half
-  // and ending in the second half (AKA, crossing repetitions)
   // divide-and-conquer algorithm - recursively divided into half
   // cout << "shift: " << shift << " - " << shift + leftLen << " : " << leftStr << " + " << rightStr << "\n\n";
   find_repetitions(leftStr, shift);
   find_repetitions(rightStr, shift + leftLen);
 
-  // cout << "z1 - leftStrReverted: " << leftStrReverted << "\n";
-  // cout << "z2 - rightStr + '#' + leftStr: " << rightStr << '#' << leftStr << "\n";
-  // cout << "z3 - leftStrReverted + '#' + rightStrReverted: " << leftStrReverted << '#' << rightStrReverted << "\n";
-  // cout << "z4 - rightStr: " << rightStr << "\n\n";
-  vector<int> z1 = z_function(leftStrReverted);
-  vector<int> z2 = z_function(rightStr + '#' + leftStr);
-  vector<int> z3 = z_function(leftStrReverted + '#' + rightStrReverted);
-  vector<int> z4 = z_function(rightStr);
+  // rossing repetitions ( O(n) ): Finds all repetitions starting in the first half and ending in the second half
+  vector<int> left1 = z(leftStrReverted);
+  vector<int> left2 = z(rightStr + '#' + leftStr);
 
-  // for (int i = 0; i < (int)z2.size(); i++)
-  //   cout << z2[i] << " ";
-  // cout << "\n\n";
+  vector<int> right1 = z(leftStrReverted + '#' + rightStrReverted);
+  vector<int> right2 = z(rightStr);
 
-  cout << "Current String: " << s << " - size: " << n << "\n";
+  // for (int i = 0; i < (int)right2.size(); i++)
+  //   cout << right2[i] << " ";
+  // cout << "zzz\n\n";
+
+  cout << "Current String: " << s << " - size: " << n << " - leftLen: " << leftLen << "\n";
   for (int i = 0; i < n; i++) {
+    // i  - fixate a specific position *cntr*
+    // l  - possible the length of the repetitions (2l). Look for all repetitions at this position
+    //      leftStr: `l = leftLen − cntr`,  rightStr: `l = cntr - leftLen + 1;`
     // k1 - the largest number in left part of the string
     // k2 - the largest number in right part of the string
     int l, k1, k2;
+    bool isLeft = i < leftLen;
 
-    cout << "i: " << i << " - leftLen: " << leftLen << "\n";
-    if (i < leftLen) {
-      l = leftLen - i;
-      k1 = get_z(z1, leftLen - i);
-      k2 = get_z(z2, rightLen + 1 + i);
+    // Once we know how to find these repetitions, iterate over all possible values for cntr from 0 to *leftLen|rightLen − 1*, and find all left crossover repetitions of length *l = leftLen|rightLen, leftLen|rightLen − 1, …, 1*
+
+    // O(n log n) algorithm contains info to solve problem below
+    // Case 1: Find the number of repetitions in a string,
+    // Case 2: Find the longest repetition in a string
+    if (isLeft) {
+      l = leftLen - i; // in decreasing order
+      k1 = getZValue(left1, l);
+      // rightLen + 1 + i: leftStr's index z fn value (in increasing order)
+      k2 = getZValue(left2, rightLen + 1 + i);
     } else {
-      l = i - leftLen + 1;
-      k1 = get_z(z3, leftLen + 1 + rightLen - 1 - (i - leftLen));
-      k2 = get_z(z4, (i - leftLen) + 1);
+      l = i - leftLen + 1; // in increasing order
+      // 1 + i + rightLen: rightStr's index z fn value (in decreasing order)
+      k1 = getZValue(right1, leftLen + n - i);
+      k2 = getZValue(right2, l);
     }
 
-    // cout << "l: " << l << "\n";
-    // cout << "k1: " << k1 << "\n";
-    // cout << "k2: " << k2 << "\n";
+    cout << "    l: " << l  << ", k1 + k2: " << k1 + k2 << ", k1: " << k1 << ", k2: " << k2 << "\n\n";
 
-    if (k1 + k2 >= l)
-      convert_to_repetitions(shift, i < leftLen, i, l, k1, k2);
+    // O (n ^ 2) store all found repetition in a vector of pairs of start and end indices.
+    // Case 3: Only run code below if you want to find Start/End position of each repetition
+    if (k1 + k2 >= l) {
+      cout << " ^^^^^^ " << "\n";
+      for (int l1 = max(1, l - k2); l1 <= min(l, k1); l1++) {
+        if (isLeft && l1 == l)
+          break;
+
+        int pos = shift + (isLeft ? i - l1 : i - l - l1 + 1);
+
+        repetitions.emplace_back(pos, pos + 2 * l - 1);
+      }
+    }
   }
-
-  cout << "\n\n";
 }
 
 int main()
 {
   string S = "abcc"; // [{2, 3}]
+  // string S = "abcabc"; // [{0, 5}]
   // string S = "abaaba"; // [{2, 3}, {0, 5}]
   // string S = "acababaee"; // [{7, 8}, {3, 6}, {2, 5}]
 
@@ -126,7 +126,6 @@ int main()
     cout << "start: " << repetitions[i].first << "\n";
     cout << "end: " << repetitions[i].second << "\n";
   }
-
 
   return 0;
 }
