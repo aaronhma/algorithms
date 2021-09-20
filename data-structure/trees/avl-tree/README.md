9dc4d42f97a8481dcda793e6ff50f7eba904f71e
Aaron Ma <aaronhma@gmail.com>
2021-09-20 15:40:12 -0700

    AVL tree

--- data-structure/trees/avl-tree/README.md
+++ data-structure/trees/avl-tree/README.md
@@ -1,50 +1,48 @@
 # AVL Tree
 
-> Named after inventors Adelson-Velsky and Landis.
-
 ## About
 
-[AVL tree](https://en.wikipedia.org/wiki/AVL_tree) is a height-balancing binary search tree. It checks the height of the left and the right sub-trees and assures that the difference is not more than *1*. This difference is called the Balance Factor.
+[AVL tree](https://en.wikipedia.org/wiki/AVL_tree) is a height-balancing binary search tree, named after Adelson-Velsky and Landis. It checks the height of the left and the right sub-trees and assures that the difference is not more than $1$. This difference is called the **Balance Factor**.
 
-All balanced binary search tree are binary search tree so the BST invariant (property or rule) holds. This means that for every node *n*, *n.left < n* and *n < n.right*.
+All balanced binary search tree are binary search trees so the BST invariant (property or rule) holds. This means that for every node $n$, $n.left < n$ and $n < n.right$.
 
 BST invariant: BST property or rule.
 It does not matter what the structure of the tree looks, all we care about is that the BST invarinat holds.
 We can shuffle, transform, rotate the values and nodes in the tree as long as the BST invariant remains satfisfied.
 
-An AVL tree is the simplest self-balancing [Binary Search Tree (BST)](../binary-search-tree/README.md). In an AVL tree, the heights of the two child subtrees of any node differ by at most *1*; if at any time they differ by more than *1*, rebalancing is done to restore this property. Lookup, insertion, and deletion all take O(log n) time in both the average and worst cases, where n is the number of nodes in the tree prior to the operation. Insertions and deletions may require the tree to be rebalanced by one or more tree rotations.
+An AVL tree is the simplest self-balancing [Binary Search Tree (BST)](../binary-search-tree/README.md). **In an AVL tree, the heights of the two child subtrees of any node differ by at most $1$; if at any time they differ by more than $1$, rebalancing is done to restore this property.** Lookup, insertion, and deletion all take $\theta(log \ n)$ time in both the average and worst cases, where $n$ is the number of nodes in the tree prior to the operation. Insertions and deletions may require the tree to be rebalanced by one or more tree rotations.
 
 ![image](./images/1.jpg)
 
-In the second tree, the left subtree of *C* has height *2* and the right subtree has height *0*, so the difference is **2**.
-In the third tree, the right subtree of *A* has height *2* and the left is missing, so it is *0*, and the difference is **2** again.
-**AVL tree permits difference (balance factor) to be only 1**
+In the second tree, the left subtree of $C$ has height $2$ and the right subtree has height $0$, so the difference is $2$.
 
+In the third tree, the right subtree of $A$ has height $2$ and the left is missing, so it is $0$, and the difference is $2$ again.
 
 ## Balance
 
-[Balanced binary search tree rotations Video](https://www.youtube.com/watch?v=q4fnJZr8ztY)
+> [How Do Rotations Work in a Balanced BST?](https://www.youtube.com/watch?v=q4fnJZr8ztY)
 
 A BST invariant (property or rule) must meet after every operation. To ensure that the tree invariant is always satisfied a series of tree
 rotations are normally applied.
 
-The threshold of the binary search tree is *<= 1*
+The threshold of the binary search tree is $\leq 1$.
 
-Balance Factor (of Current Node) = Height(Left-Subtree) - Height(Right-Subtree)
-AVL Tree = abs(Balance Factor) <= 1
-If the difference in the height of left and right sub-trees is more than 1, the tree is balanced using some rotation techniques.
+$$\text{balance factor of node } x = height(\text{left subtree}) - height(\text{right subtree})$$
 
-H is for Height:
-H(EmptyTree) = -1  (Null)
-H(SingleNode) = 0
-H(CurrentNode) = max(H(TreeLeft), H(TreeRight) ) + 1
+If the difference in the height of left and right sub-trees is more than $1$, the tree is balanced using rotation techniques described below.
 
-## 4 kinds of Rotations
+$h$ is for the height of the BST:
 
-Rotation should only focus on two nodes, always using rotation to maintain the balance of binary search tree.
+$$h(\text{empty tree}) = h(\text{tree with } 1 \text{ node}) = 0$$
+$$h(\text{current node}) = 1 + max(h(\text{left subtree}), h(\text{left subtree}))$$
+
+## Types of Rotations
+
+Rotation should only focus on two nodes, always using rotations to maintain the balance of the binary search tree.
 
 *Left-Heavy*: Right, left-right
-*Right-Heavy*: Left, right-left.
+
+*Right-Heavy*: Left, right-left
 
 1. *Left-Heavy*: Right Rotation
 
--- data-structure/trees/avl-tree/avl-tree.cpp
+++ data-structure/trees/avl-tree/avl-tree.cpp
@@ -1,39 +1,6 @@
-// TODO: https://www.geeksforgeeks.org/avl-tree-set-1-insertion/ & https://www.youtube.com/watch?v=q4fnJZr8ztY
 #include <bits/stdc++.h>
 
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef string str;
-typedef vector<int> vi;
-typedef vector<bool> vb;
-typedef vector<ll> vll;
-typedef vector<char> vc;
-typedef vector<str> vstr;
-typedef vector<vi> vvi;
-typedef pair<int, int> pii;
-typedef vector<pii> vpii;
-
-#define f first
-#define s second
-#define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
-#define PB push_back
-#define sz(x) (int)x.size()
-#define rtn return
-#define rsz resize
-#define ins insert
-#define ft front()
-#define bk back()
-#define LB lower_bound
-#define UB upper_bound
-#define BINS binary_search
-#define IT(x) for (auto it = begin(x); it != end(x); it++)
-#define RIT(x) for (auto it = rbegin(x); it != rend(x); it++)
-#define FORE(i, a, b) for (int i = a; i < b; i++)
-#define FORI(i, a, b) for (int i = a; i <= b; i++)
-#define each(i, x) for (auto &i : x)
 
 struct Node
 {
@@ -45,12 +12,34 @@ struct Node
   Node(int V, Node *L, Node *R) : val(V), left(L), right(R) {}
 };
 
+Node *rightRotate(Node *root)
+{
+  Node *tmp = root->left;
+  Node *rr = tmp->right;
+
+  tmp->right = root;
+  root->left = rr;
+
+  return tmp;
+}
+
+Node *leftRotate(Node *root)
+{
+  Node *tmp = root->right;
+  Node *rl = tmp->left;
+
+  tmp->left = root;
+  root->right = rl;
+
+  return tmp;
+}
+
 int height(Node *root)
 {
   if (!root)
     return 0;
 
-  return root->height;
+  return 1 + max(height(root->left), height(root->right));
 }
 
 Node *newNode(int x)
@@ -60,32 +49,9 @@ Node *newNode(int x)
   return node;
 }
 
-Node *rightRotate(Node *root)
+int getBalance(Node *root)
 {
-  Node *tmp = root->left;
-  Node *rr = tmp->right;
-
-  tmp->right = root;
-  root->left = rr;
-
-  root->height = 1 + max(height(root->left), height(root->right));
-  tmp->height = 1 + max(height(tmp->left), height(tmp->right));
-
-  return tmp;
-}
-
-Node *leftRotate(Node *root)
-{
-  Node *tmp = root->left;
-  Node *rl = tmp->right;
-
-  tmp->right = root;
-  root->left = rl;
-
-  root->height = 1 + max(height(root->left), height(root->right));
-  tmp->height = 1 + max(height(tmp->left), height(tmp->right));
-
-  return tmp;
+  return height(root->left) - height(root->right);
 }
 
 Node *insert(Node *root, int x)
@@ -101,42 +67,75 @@ Node *insert(Node *root, int x)
   else
     root->right = insert(root->right, x);
 
-  root->height = 1 + max(height(root->left), height(root->right));
+  int balance = getBalance(root);
 
-  int balance = root->left->height - root->right->height;
-
-  if (balance > 1 && x < root->left->val) return rightRotate(root);
-  // if (balance < -1 && x > root->right->val) return leftRotate(root);
-
-  // if (balance > 1 && x > root->left->val) {
-  //   root->left = leftRotate(root->left);
-  //   return rightRotate(root);
-  // }
-
-  // if (balance < -1 && x < root->right->val) {
-  //   root->right = rightRotate(root->right);
-  //   return leftRotate(root);
-  // }
+  if (balance > 1)
+  {
+    if (getBalance(root->left) < 0)
+      root->left = leftRotate(root->left); // Left-Right Case
+    return rightRotate(root);              // Left Case
+  }
+  else if (balance < -1)
+  {
+    if (getBalance(root->right) > 0)
+      root->right = rightRotate(root->right); // Right-Left Case
+    return leftRotate(root);                  // Right Case
+  }
 
   return root;
 }
 
-void preOrder(Node *root)
+Node *deleteNode(Node *root, int x)
+{
+  if (!root)
+    return root;
+
+  if (x < root->val)
+    root->left = deleteNode(root->left, x);
+  else if (x > root->val)
+    root->right = deleteNode(root->right, x);
+  else
+  {
+    if (!root->right)
+    {
+      Node *tmp = root->left;
+      root = NULL;
+      return tmp;
+    }
+    else if (!root->left)
+    {
+      Node *tmp = root->right;
+      root = NULL;
+      return tmp;
+    }
+
+    Node *tmp = root->right;
+
+    while (!tmp->left) tmp = tmp->left;
+
+    root->val = tmp->val;
+    root->right = deleteNode(root->right, tmp->val);
+  }
+
+  return root;
+}
+
+void inOrder(Node *root)
 {
   if (!root)
     return;
+  inOrder(root->left);
   cout << root->val << " ";
-  preOrder(root->left);
-  preOrder(root->right);
+  inOrder(root->right);
 }
 
 int main()
 {
-  Node *root = new Node;
+  Node *root = new Node(10);
 
   /* Constructing tree given in
     the above figure */
-  root = insert(root, 10);
+  root = insert(root, 10); // Will not add 10 again to the AVL tree, as no duplicated are allowed in this implementation.
   root = insert(root, 20);
   root = insert(root, 30);
   root = insert(root, 40);
@@ -150,9 +149,14 @@ int main()
             / \ \
         10 25 50
     */
-  cout << "Preorder traversal of the "
-          "constructed AVL tree is \n";
-  preOrder(root);
+  cout << "Inorder traversal of the constructed AVL tree: \n";
+  inOrder(root);
+  cout << "\n";
+
+  root = deleteNode(root, 10);
+
+  cout << "AVL tree after removing node 10: \n";
+  inOrder(root);
   cout << "\n";
 
   return 0;
